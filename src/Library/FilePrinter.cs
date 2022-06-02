c88697d9d74448a983338ddf7f0feeac083b72d1
Stibsosa <stib.sosa@correo.ucu.edu.uy>
2022-06-02 20:35:31 -0300

    commit

--- src/Library/FilePrinter.cs
+++ src/Library/FilePrinter.cs
@@ -2,9 +2,9 @@ using System.IO;
 
 namespace Full_GRASP_And_SOLID
 {
-    public class FilePrinter : IPrinter
+    public class FilePrinter : IPrinter 
     {
-        public void PrintRecipe(Recipe recipe)
+        public void PrintRecipe(IRecipe recipe)
         {
             File.WriteAllText("Recipe.txt", recipe.GetTextToPrint());
         }
